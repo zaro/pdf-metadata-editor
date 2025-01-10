e3cc2294b1dd43fe96d1caf81b45abd477208828
Svetlozar Argirov <zarrro@gmail.com>
2025-01-10 12:45:12 +0200

    Remove obsolete files

--- package/all/README.md
+++
@@ -1,1 +0,0 @@
-Windows launcher http://winrun4j.sourceforge.net/
--- package/all/winrun4j/replace_exe.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-SRC_FILE=WinRun4J.exe
-
-for file in PdfMetadataEditor.exe PdfMetadataEditor64.exe pmedit-cli.exe pmedit-cli64.exe; do
-
-    cp -v $SRC_FILE $file
-
-done
