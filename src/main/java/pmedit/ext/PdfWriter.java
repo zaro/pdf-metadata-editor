d614fb66e7ba0037aaf7904b8de72790a022f5cc
Svetlozar Argirov <zarrro@gmail.com>
2025-06-05 09:51:55 +0300

    Show minimum version supported for compression

--- src/main/java/pmedit/ext/PdfWriter.java
+++ src/main/java/pmedit/ext/PdfWriter.java
@@ -1,26 +1,38 @@
 package pmedit.ext;
 
+import org.apache.pdfbox.pdfwriter.compress.COSWriterCompressionPool;
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import pmedit.FileOptimizer;
 import pmedit.pdf.CompressionAndOptimisation;
-import pmedit.preset.PresetValues;
 
 import java.io.*;
 
 public class PdfWriter {
+    static Logger LOG = LoggerFactory.getLogger(PdfWriter.class);
+    static public final float COMPRESSION_MIN_VERSION = 1.5f;
     protected PDDocument pdDocument;
+
+    public static float getCompressionMinimumSupportedVersion() {
+        return COSWriterCompressionPool.MINIMUM_SUPPORTED_VERSION;
+    }
+
+
     public PdfWriter(PDDocument document){
         this.pdDocument = document;
     }
 
     public void write(File file, int pdfBoxCompression) throws IOException{
         final CompressionAndOptimisation compressParameters= new CompressionAndOptimisation(pdfBoxCompression);
+        LOG.debug("write(File) {}", file);
         pdDocument.save(file, compressParameters.getCompressParameters());
 
     }
 
     public <T extends CompressionAndOptimisation> void write(OutputStream output) throws IOException {
         final CompressionAndOptimisation compressParameters= new CompressionAndOptimisation(FileOptimizer.getPdfBoxCompression());
+        LOG.debug("write(OutputStream)");
         pdDocument.save(output, compressParameters.getCompressParameters());
     }
 }
